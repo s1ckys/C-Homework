2219fea081424e181dc295c1405268697e55cfce
s1ckys <131174539+s1ckys@users.noreply.github.com>
2025-03-09 17:44:58 +0100

    Add files via upload

---
+++ Homework2/StudentGroup/Program.cs
@@ -0,0 +1,33 @@
+﻿string[] studentsG1 = new string[] {"Zdravko", "Petko", "Stanko", "Branko", "Trajko"};
+string[] studentsG2 = new string[] {"Marko", "Darko", "Zarko", "Gjorgji", "Pero"};
+
+Console.WriteLine("Enter student group: ( there are 1 and 2 )");
+bool success = int.TryParse(Console.ReadLine(), out int groupNumber);
+if (success)
+{
+    if (groupNumber == 1)
+    {
+        Console.WriteLine("The students in G1 are: ");
+        foreach (string student in studentsG1)
+        {
+            Console.WriteLine(student);
+        }
+    }
+    else if (groupNumber == 2)
+    {
+        Console.WriteLine("The students in G2 are: ");
+        foreach (string student in studentsG2)
+        {
+            Console.WriteLine(student);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please enter valid group number!");
+    }
+}
+else
+{
+    Console.WriteLine("Please enter valid number!");
+}
+Console.ReadLine();
---
+++ Homework2/SumOfEven/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] numberArray = new int[] {};
+int sum = 0;
+
+Console.WriteLine("Input 6 integers and get the sum of all the even integers.\n");
+bool successInputOfAllInts = true;
+for(int i = 0; i<=5; i++)
+{
+    Console.WriteLine("Enter integer no." + (i + 1));
+    bool success = int.TryParse(Console.ReadLine(), out int number);
+    if (success)
+    {
+        Array.Resize(ref numberArray, numberArray.Length + 1);
+        numberArray[i] = number;
+    }
+    else
+    {
+        Console.WriteLine("Please enter a valid number!!");
+        successInputOfAllInts = false;
+        break;
+    }
+}
+
+if (successInputOfAllInts)
+{
+    foreach (int item in numberArray)
+    {
+        if (item % 2 == 0)
+        {
+            sum += item;
+        }
+    }
+
+    Console.WriteLine("The result is " + sum);
+}
+Console.ReadLine();
